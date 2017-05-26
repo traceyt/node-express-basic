32097c9820f8a51ae78561784dac0f71eaeddf66
Tracey Trewin <tracey@trewin.com>
2017-05-26 15:38:18 -0700

    updated with jshint config file

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('got a request from the server for requests!!');
+    res.send('got a request from the server for requests again!!');
 });
 
 export default app;
