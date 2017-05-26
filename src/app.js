c22fa41775877cb2390502ae6ddfe080a53e26a8
Tracey Trewin <tracey@trewin.com>
2017-05-26 11:11:02 -0700

    got basic node express server running

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', function(req, res) {
+    res.send('got a request');
+});
+
+app.listen(PORT, function() {
+    console.log('listening for requests');
+});
